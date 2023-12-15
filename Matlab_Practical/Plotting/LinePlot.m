2c785aec2639f3b6f971429c1d232adb4a37f46e
SaifAlam <kaif0350@gmail.com>
2023-12-15 23:29:31 +0530

    Plotting

---
+++ Matlab_Practical/Plotting/LinePlot.m
@@ -0,0 +1,13 @@
+Program :
+1. Line Plot:
+A line plot is used to represent data points as a series of connected lines.
+x = [1, 2, 3, 4, 5];
+y = [2, 3, 1, 4, 7];
+plot(x, y);
+title('Line Plot');
+xlabel('X-axis Label');
+ylabel('Y-axis Label');
+ 
+
+
+ 
---
+++ Matlab_Practical/Plotting/ScatterPlot.m
@@ -0,0 +1,8 @@
+2. Scatter Plot:
+A scatter plot is used to visualize the relationship between two variables.
+x = [1, 2, 3, 4, 5];
+y = [2, 3, 1, 4, 7];
+scatter(x, y);
+title('Scatter Plot');
+xlabel('X-axis Label');
+ylabel('Y-axis Label');
